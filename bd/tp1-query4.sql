adef0477c7d9b269d7d3a254967eb2a43992b325
Juan Cruz Sosa <nirvguy@gmail.com>
2018-10-16 23:19:33 -0300

    Cambio forma de sacar los top 1

--- bd/tp1-query4.sql
+++ bd/tp1-query4.sql
@@ -3,9 +3,8 @@ FROM tp1."Incidente" i
 JOIN tp1."OficialSeInvolucro" oci ON oci."idIncidente" = i."idIncidente"
 JOIN tp1."Oficial" o ON oci.placa = o.placa
 GROUP BY o.placa, o.dni, o.nombre, o.apellido
-HAVING COUNT(distinct i."idIncidente") = (SELECT MAX(incidentes_count) from 
-( SELECT count (distinct i."idIncidente") incidentes_count
-FROM tp1."Incidente" i
-JOIN tp1."OficialSeInvolucro" oci ON oci."idIncidente" = i."idIncidente"
-JOIN tp1."Oficial" o ON oci.placa = o.placa
-GROUP BY o.placa ) inc_count);											
+HAVING COUNT(distinct i."idIncidente") >= ALL (SELECT COUNT(distinct i2."idIncidente")
+											   FROM tp1."Incidente" i2
+											   JOIN tp1."OficialSeInvolucro" oci2 ON oci2."idIncidente" = i2."idIncidente"
+											   JOIN tp1."Oficial" o2 ON oci2.placa = o2.placa
+											   GROUP BY o2.placa);
