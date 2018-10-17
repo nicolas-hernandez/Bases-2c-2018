5d178b308c62e697dd5cbf187ddf731b8334cd89
Juan Cruz Sosa <nirvguy@gmail.com>
2018-10-16 23:01:43 -0300

    Query 1 - Unifica columnas

--- bd/tp1-query1.sql
+++ bd/tp1-query1.sql
@@ -1,13 +1,32 @@
-SELECT i."idIncidente", ti.nombre, i.fecha, i.calle_1, i.calle_2, d.calle, d.altura, b.nombre, o.placa, o.nombre, o.apellido, o.rango, ro.descripcion,
-c.dni, c.nombre, c.apellido, rc.nombre
+SELECT i."idIncidente" as "idIncidente",
+	   i.fecha,
+	   i.calle_1 as calle_1,
+	   i.calle_2 as calle_2,
+		'Oficial' as tipo,
+        o.placa as "placaOficial",
+		o.dni as dni,
+		o.nombre as nombre,
+		o.apellido as apellido,
+		ro.descripcion as "rol"
 FROM tp1."Incidente" i
-JOIN tp1."TipoIncidente" ti ON i."idTipoInicidente" = ti."idTipoInicidente"
 JOIN tp1."OficialSeInvolucro" osi ON i."idIncidente" = osi."idIncidente"
 JOIN tp1."Oficial" o ON osi.placa = o.placa
-JOIN tp1."SeInvolucraron" si ON i."idIncidente" = si."idIncidente"
-JOIN tp1."Civil" c ON si.dni = c.dni 
 JOIN tp1."RolOficial" ro ON osi."idResponsabilidad" = ro."idResponsabilidad"
+WHERE i.fecha < '2018-10-07' and i.fecha > '2018-09-15'
+UNION
+SELECT i."idIncidente" AS "idIncidente",
+	   i.fecha,
+	   i.calle_1 as calle_1,
+	   i.calle_2 as calle_2,
+		'Civil' as tipo,
+        NULL as "placaOficial",
+		c.dni as dni,
+		c.nombre as nombre,
+		c.apellido as apellido,
+		rc.nombre as "rol"
+FROM tp1."Incidente" i
+JOIN tp1."SeInvolucraron" si ON i."idIncidente" = si."idIncidente"
+JOIN tp1."Civil" c ON si.dni = c.dni
 JOIN tp1."RolCivil" rc ON si."idRolCivil" = rc."idRolCivil"
-JOIN tp1."Direccion" d ON i."idDireccion" = d."idDireccion"
-JOIN tp1."Barrio" b ON d."idBarrio" = b."idBarrio"
-WHERE i.fecha < '2018-10-07' and i.fecha > '2018-09-15';
+WHERE i.fecha < '2018-10-07' and i.fecha > '2018-09-15'
+ORDER BY 1 ASC, 5 ASC
